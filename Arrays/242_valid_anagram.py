a7518b5c24cb8fddb21d88b2c74e4d8aed86e318
seetha04x <seetha04x@gmail.com>
2026-01-11 01:38:16 +0530

    Update 242_valid_anagram.py

--- Arrays/242_valid_anagram.py
+++ Arrays/242_valid_anagram.py
@@ -1,3 +1,7 @@
+#created a hashmap usimg dictionary to store an element and its occurrence
+#the first string is mapped to the dictionary
+#then second string is compared like in a turing machine for palindrome(cancelling out)
+#if all are cancelled out(count=0), then the strings are anagram
 class Solution:
     def isAnagram(self, s: str, t: str) -> bool:
         if (len(s)!=len(t)):

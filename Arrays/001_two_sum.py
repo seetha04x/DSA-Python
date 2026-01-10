a82e4ef7c74ca2d1d9af1ece94d87927c9272eb6
seetha04x <seetha04x@gmail.com>
2026-01-11 01:41:38 +0530

    Update 001_two_sum.py

--- Arrays/001_two_sum.py
+++ Arrays/001_two_sum.py
@@ -1,3 +1,9 @@
+#hash map is created
+#enumerate function used to store indices and values of num
+#key is found, and the logic is to find the key in the hashmap
+#the key is matched to the elements before it,if key is found, the indices(value) are returned 
+#if not found the element is added to hashmap
+
 class Solution:
     def twoSum(self, nums: List[int], target: int) -> List[int]:
         hashMap={}

b82971c74db14cebfe93d9b6a6f7cdcaab473be6
seetha04x <seetha04x@gmail.com>
2026-01-10 23:24:26 +0530

    Add contains duplicate(O(n) using Hash Set)

---
+++ 217_contains_duplicate.py
@@ -0,0 +1,11 @@
+class Solution:
+    def containsDuplicate(self, nums: List[int]) -> bool:
+        hash=set()
+        for i in nums:
+            if i in hash:
+                return True
+            hash.add(i)    
+        return False        
+            
+
+            

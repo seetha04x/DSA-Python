ae3457fb9a44b6bf0242e74f384cbec1dc33d759
seetha04x <seetha04x@gmail.com>
2026-01-10 23:46:03 +0530

    Update 217_contains_duplicate.py

--- Arrays/217_contains_duplicate.py
+++ Arrays/217_contains_duplicate.py
@@ -1,3 +1,6 @@
+#created a hashMap using set to store all unique elements.
+#the array elements are traversed and if the element is found in hashMap, then duplicate elements exists.
+#if not, the element is added to hashMap
 class Solution:
     def containsDuplicate(self, nums: List[int]) -> bool:
         hash=set()

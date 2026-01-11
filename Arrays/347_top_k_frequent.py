8a56c89034f85fa53139d3bfb45db11aa9a88ef8
seetha04x <seetha04x@gmail.com>
2026-01-11 23:16:37 +0530

    Add Top K Frequent Elements

---
+++ Arrays/347_top_k_frequent.py
@@ -0,0 +1,18 @@
+from collections import Counter
+import heapq
+class Solution:
+    def topKFrequent(self, nums: List[int], k: int) -> List[int]:
+        freq=Counter(nums) 
+        heap=[]
+        for num, count in freq.items():
+            heapq.heappush(heap, (count,num))
+            if len(heap)>k:
+                heapq.heappop(heap)
+        return [num for count, num in heap]    
+                       
+
+
+
+
+        
+     

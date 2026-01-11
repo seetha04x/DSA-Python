171a5b3bd3daed219870ef7a5d0f826616e3828b
seetha04x <seetha04x@gmail.com>
2026-01-12 00:38:53 +0530

    Add Comment in Top K Frequent Element

--- Arrays/347_top_k_frequent.py
+++ Arrays/347_top_k_frequent.py
@@ -1,3 +1,7 @@
+#frequency is counted and stored in dictionary using Counter()
+#using freq.items(), the num and count is accessed and pushed to min heap
+#whenever more than k elements are (say k=2), ie, when 3 elements are added first element is popped which is the minimum element and 2 element are kept intact ie, the top k frequent elements
+
 from collections import Counter
 import heapq
 class Solution:
